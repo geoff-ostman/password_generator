1f4b4e93202a262481132b072fc1f470c1d3112f
Geoff Ostman <81042162+geoff-ostman@users.noreply.github.com>
2021-05-09 14:42:37 -0500

    What am I doing?

--- generator.js
+++ generator.js
@@ -1,17 +1,28 @@
 // Assignment code here
-
+I have no idea what I am doing...
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var length = 8,
+  var generatePassword = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+  retVal = "";
+
+for (var i = 0, n = charset.length; i < length; ++i) {
+  retVal += charset.charAt(Math.floor(Math.random() * n));
+}
+return retVal;
+}
 
   passwordText.value = password;
 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+console.log (writePassword[i]) 
+for (var i=0; i < writePassword.length;i++)
+i < writePassword.length
